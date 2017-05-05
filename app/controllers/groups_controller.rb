55fc2b46ad1be3a5b4d55e6a450a7d2afae67dae
catcher <zrtspp@126.com>
2017-05-05 23:21:20 +0000

    add validation to title

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -17,10 +17,13 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
-    @group.save
 
-      redirect_to groups_path
+    if @group.save
+      redirect_to group_path
+    else
+      render :new
   end
+end
 
   def update
     @group = Group.find(params[:id])
