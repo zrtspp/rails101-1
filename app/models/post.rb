c48f9453fe0dede5a7f16a8e284a0f98eda92160
catcher <zrtspp@126.com>
2017-05-07 21:28:05 +0000

    add post and hook group/user relation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,4 @@
 class Post < ApplicationRecord
   belongs_to :user
-  belongs_to :group
-
-  validates :content, presence: true
-
-  scope :recent, -> { order("created_at DESC")}
+  belongs_to :group  
 end
